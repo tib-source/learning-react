ed0afcb2cbde4e2bc97a5a2b68816c74e4b1919b
Tsquare25 <tibebe1234t@gmail.com>
2021-10-04 14:26:00 +0100

    added json server but havent worked on it yet

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -4,26 +4,7 @@ import AddTask from "./components/AddTask";
 import { useState } from "react";
 function App() {
   const [showAdd, setShowAdd] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: false,
-    },
-    {
-      id: 3,
-      text: "Calling Mengistu",
-      day: "Oct 5th at 1:30pm",
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   // create Task
   const createTask = (newTask) => {
--- task-tracker/src/components/AddTask.js
+++ task-tracker/src/components/AddTask.js
@@ -1,22 +1,22 @@
 import { useState } from "react/cjs/react.development";
 
-const AddTask = ({createTask}) => {
+const AddTask = ({ createTask }) => {
     const [text, setText] = useState("");
     const [day, setDay] = useState("");
     const [reminder, setReminder] = useState(false);
 
-    const saveClicked = (e) =>{
+    const saveClicked = (e) => {
         console.log(e)
         console.log("IM RUNNING")
         e.preventDefault()
 
-        if(!text){
+        if (!text) {
             alert("Please write a task before saving")
-            return 
+            return
         }
 
-        let id = Math.floor(Math.random()*9000)
-        let newTask = {id, text, day, reminder}
+        let id = Math.floor(Math.random() * 9000)
+        let newTask = { id, text, day, reminder }
 
         createTask(newTask)
         setText("")
@@ -27,17 +27,17 @@ const AddTask = ({createTask}) => {
         <form className="add-form">
             <div className="form-control">
                 <label>Task</label>
-                <input type="text" name="task" placeholder="Add your task" value={text} onChange={(e)=>setText(e.target.value)} />
+                <input type="text" name="task" placeholder="Add your task" value={text} onChange={(e) => setText(e.target.value)} />
             </div>
             <div className="form-control">
                 <label>Date</label>
-                <input type="text" name="date" placeholder="Due date" value={day} onChange={(e)=>setDay(e.target.value)} />
+                <input type="text" name="date" placeholder="Due date" value={day} onChange={(e) => setDay(e.target.value)} />
             </div>
             <div className="form-control form-control-check">
                 <label>Set Reminder</label>
-                <input type="checkbox" name="reminder" checked={reminder} value={reminder} onChange={(e)=>setReminder(e.currentTarget.checked)} />
+                <input type="checkbox" name="reminder" checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
             </div>
-            <input type="submit" value="Save" className="btn btn-block" onClick={(e)=>saveClicked(e)} />
+            <input type="submit" value="Save" className="btn btn-block" onClick={(e) => saveClicked(e)} />
         </form>
     );
 };
